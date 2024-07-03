206503e2aaa9739d42dd8959ad8cd93fca8ddd96
Ricard RUBIO <ricard.rubio@soprasteria.com>
2024-07-03 12:38:23 +0200

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    return [
+        {
+            name: 'HTML',
+            level: '90%'
+        },
+        {
+            name: 'CSS',
+            level: '80%'
+        },
+        {
+            name: 'JavaScript',
+            level: '70%'
+        },
+        {
+            name: 'React',
+            level: '60%'
+        },
+        {
+            name: 'Node.js',
+            level: '50%'
+        }
+    ]
+}
